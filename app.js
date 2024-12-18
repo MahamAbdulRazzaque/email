aa5bb0d8224efa6c38e7812646e20d12b0dac61f
Maham Abdul Razzaque <mahamabdulrazzaque@gmail.com>
2024-12-18 17:04:59 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+
+function field() {
+   let email = document.getElementById("email").value;
+  if (email===""){
+    alert("Enter your Email")
+    return;
+  }
+   else{
+    alert("Email is Verified")
+  }    
+}
